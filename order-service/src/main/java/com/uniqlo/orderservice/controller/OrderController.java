d6b7d24c8cf6faf1b206cf92c10f3ea768222b56
leexizhe <leexizhe17@gmail.com>
2023-08-02 23:28:31 +0800

    basic circuit breaker

--- order-service/src/main/java/com/uniqlo/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/uniqlo/orderservice/controller/OrderController.java
@@ -2,12 +2,15 @@ package com.uniqlo.orderservice.controller;
 
 import com.uniqlo.orderservice.dto.OrderRequest;
 import com.uniqlo.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@Slf4j
 @RequestMapping("/api/order")
 public class OrderController {
 
@@ -15,7 +18,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void placeOrder(@RequestBody OrderRequest orderRequest) {
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+
+    public String fallbackMethod(Throwable throwable) {
+        log.debug("Inventory Service is down!", throwable);
+        return "Try again later";
     }
 }
